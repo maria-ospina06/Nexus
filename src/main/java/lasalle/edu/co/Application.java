5ed27c731c73492859754b3a315b88c96dd4a49d
maria ospina <maospina@lasalle.edu.co>
2025-09-08 18:52:49 -0500

    Prueba 2

--- src/main/java/lasalle/edu/co/Application.java
+++ src/main/java/lasalle/edu/co/Application.java
@@ -26,6 +26,7 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         System.out.println("Iniciando 1...");
+        System.out.println("Iniciando 2...");
         SpringApplication.run(Application.class, args);
     }
 
